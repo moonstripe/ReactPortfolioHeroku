8932c3863cb3c76424de8fafa23460939eb57822
Kojin Glick <kojinglick@gmail.com>
2021-01-19 10:42:34 -0800

    static.json/index.js

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(routes);
 
+// app.get("*", (req, res) => {
+//     let url = path.join(__dirname, '../client/build', 'index.html');
+//     if (!url.startsWith('/app/')) // since we're on local windows
+//         url = url.substring(1);
+//     res.sendFile(url);
+// });
+
 // Connect to the Mongo DB
 mongoose.connect(
     process.env.MONGODB_URI,
