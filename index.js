926464d8429411993274b3f33635ef31e7144f34
Kojin Glick <kojinglick@gmail.com>
2021-01-19 23:14:25 -0800

    heroku pls

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const PORT = process.env.PORT || 3001;
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('/build'));
 }
 app.use(express.static(publicPath));
 
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build'));
+// });
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
