c06fc2c9d8a9e1f5087fdcd276b349263b0e5e23
Kojin Glick <kojinglick@gmail.com>
2021-01-19 23:20:11 -0800

    pls last push

--- index.js
+++ index.js
@@ -7,19 +7,12 @@ const path = require('path');
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-console.log(publicPath);
 const PORT = process.env.PORT || 3001;
 
-
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/build'));
+    app.use(express.static('client/build'));
 }
 app.use(express.static(publicPath));
-
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build'));
-// });
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
