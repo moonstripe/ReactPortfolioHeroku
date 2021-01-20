8513cdbad7315c310072a887b3012e1d58d93634
Kojin Glick <kojinglick@gmail.com>
2021-01-19 16:30:31 -0800

    damn son where'd you find this

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const PORT = process.env.PORT || 3001;
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 app.use(express.static(publicPath));
+
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/client/build/index.html'));
+// });
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
