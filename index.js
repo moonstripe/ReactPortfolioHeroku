bf25e7918e4e1e51b5579547d062163c506bd1d6
Kojin Glick <kojinglick@gmail.com>
2021-01-20 16:32:02 -0800

    hmm

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const PORT = process.env.PORT || 3001;
 
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 app.use(express.static(publicPath));
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
 app.use(routes);
 
 // Connect to the Mongo DB
